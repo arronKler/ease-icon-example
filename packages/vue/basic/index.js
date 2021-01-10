a293ea73c6eb1945b1d0327b8c65102d8644dce6
arronKler <panzer9@126.com>
2021-01-10 23:52:55 +0800

    feat: add babel related stuff

---
+++ packages/vue/basic/index.js
@@ -0,0 +1,12 @@
+/* Auto-generated code by ease-icon */
+
+import MailOpen from "./icons/MailOpen"
+export { MailOpen }
+
+export default {
+  install(Vue, options = {}) {
+    const prefix = options.prefix || "I"
+
+    Vue.component(prefix + MailOpen.name, MailOpen)
+  },
+}
