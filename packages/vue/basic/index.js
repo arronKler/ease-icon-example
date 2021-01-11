4000280eb070d3e2968bd9fd8546812306f1ed69
arronKler <zouyawei@luojilab.com>
2021-01-11 21:27:39 +0800

    feat: test iconfont generator

--- packages/vue/basic/index.js
+++ packages/vue/basic/index.js
@@ -1,12 +1,22 @@
 /* Auto-generated code by ease-icon */
 
 import MailOpen from "./icons/MailOpen"
-export { MailOpen }
+import Share from "./icons/Share"
+import Service from "./icons/Service"
+import Cart from "./icons/Cart"
+import Close from "./icons/Close"
+import Phone from "./icons/Phone"
+export { MailOpen, Share, Service, Cart, Close, Phone }
 
 export default {
   install(Vue, options = {}) {
     const prefix = options.prefix || "I"
 
     Vue.component(prefix + MailOpen.name, MailOpen)
+    Vue.component(prefix + Share.name, Share)
+    Vue.component(prefix + Service.name, Service)
+    Vue.component(prefix + Cart.name, Cart)
+    Vue.component(prefix + Close.name, Close)
+    Vue.component(prefix + Phone.name, Phone)
   },
 }
